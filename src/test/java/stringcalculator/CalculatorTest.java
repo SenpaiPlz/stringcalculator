fa04d5019cd0febfaccd29bec1c8e2f8846e34e5
senpai <senpai@plznotice.me>
2016-10-13 20:02:15 +0000

    Added tests for strings containing a single integer

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -12,4 +12,14 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
+
+	@Test
+	public void testSingleCorrectInputString() {
+		assertEquals(1, Calculator.add("1"));
+		assertEquals(2, Calculator.add("2"));
+		assertEquals(3, Calculator.add("3"));
+		assertEquals(5, Calculator.add("5"));	
+		assertEquals(8, Calculator.add("8"));
+
+	}
 }
