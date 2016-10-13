109038b838f5a69440ecb68c1ab5c2b1cdcb4d71
senpai <senpai@plznotice.me>
2016-10-13 22:40:33 +0000

    added functionality to throw exception for illegal negative values

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,20 +1,29 @@
 package stringcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator { 
 
 	public static int add(String in) {
 		if(in == "")
 			return 0;
 
-		if(in.contains(",") || in.contains("\n")) {
-			String[] temp = in.split(",|\n");
-			int sum = 0;
-			for(int i = 0; i < temp.length; i++) {
-				sum += Integer.parseInt(temp[i]);
+		String[] temp = in.split(",|\n");
+		ArrayList<Integer> negatives = new ArrayList<Integer>();
+		int sum = 0;
+		for(int i = 0; i < temp.length; i++) {
+			int tempint = Integer.parseInt(temp[i]);
+			if(tempint < 0) {
+				negatives.add(tempint);
 			}
-			return sum;
+			else
+				sum += tempint;
 		}
-		return Integer.parseInt(in);
+		if(negatives.size() != 0) {
+			throw new IllegalArgumentException("Negatives not allowed: " + negatives);
+		}
+		return sum;
 	}
 
 }
