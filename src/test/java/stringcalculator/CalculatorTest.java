0495ddbb0ec1262bd5538a5aba85f5a8034cf32b
senpai <senpai@plznotice.me>
2016-10-13 20:09:08 +0000

    added tests for strings containing two correct integet values

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -14,12 +14,21 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testSingleCorrectInputString() {
+	public void testSingleCorrectInput() {
 		assertEquals(1, Calculator.add("1"));
 		assertEquals(2, Calculator.add("2"));
 		assertEquals(3, Calculator.add("3"));
 		assertEquals(5, Calculator.add("5"));	
 		assertEquals(8, Calculator.add("8"));
 
+	}
+
+	@Test
+	public void testTwoCorrectInputs() {
+		assertEquals(2, Calculator.add("1,1"));
+		assertEquals(3, Calculator.add("1,2"));
+		assertEquals(5, Calculator.add("2,3"));
+		assertEquals(8, Calculator.add("3,5"));
+		assertEquals(13, Calculator.add("5,8"));
 	}
 }
