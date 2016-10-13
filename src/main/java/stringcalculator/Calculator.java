53fd0690092cb14d9c1d1c88510c1c6f62fb870d
senpai <senpai@plznotice.me>
2016-10-13 20:57:37 +0000

    Calculator now accepts multiple terms with , as a seperator

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,9 +1,19 @@
 package stringcalculator;
 
-public class Calculator {
+public class Calculator { 
+
 	public static int add(String in) {
 		if(in == "")
 			return 0;
+		if(in.contains(",")) {
+			String[] temp = in.split(",");
+			int sum = 0;
+			for(int i = 0; i < temp.length; i++) {
+				sum += Integer.parseInt(temp[i]);
+			}
+			return sum;
+		}
 		return Integer.parseInt(in);
 	}
+
 }
