5ac7b650e8df88f4439ce393d10e2dc727263007
senpai <senpai@plznotice.me>
2016-10-13 22:07:35 +0000

    now differentiates between newline and , seperator

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -5,8 +5,9 @@ public class Calculator {
 	public static int add(String in) {
 		if(in == "")
 			return 0;
-		if(in.contains(",")) {
-			String[] temp = in.split(",");
+
+		if(in.contains(",") || in.contains("\n")) {
+			String[] temp = in.split(",|\n");
 			int sum = 0;
 			for(int i = 0; i < temp.length; i++) {
 				sum += Integer.parseInt(temp[i]);
